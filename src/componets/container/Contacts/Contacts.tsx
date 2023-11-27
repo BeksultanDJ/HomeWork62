66f0b9443eb9d8f5fc06a598a3329da67663894e
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-27 17:27:31 +0600

    сделал страницу контакты и добавил ссылки на соцсети

--- src/componets/container/Contacts/Contacts.tsx
+++ src/componets/container/Contacts/Contacts.tsx
@@ -5,7 +5,12 @@ function Contacts() {
 
     return (
         <>
-            <h1>Contacts page</h1>
+            <div className="linksBlock container">
+                <strong>Links:</strong>
+                <a href="https://www.instagram.com/5kein/">Inst: SKEIN</a>
+                <a href="https://www.instagram.com/fixiekg/">Inst: Fixie.kg</a>
+                <h4>Подписывайтесь, чтобы следить за событиями</h4>
+            </div>
         </>
     )
 }
