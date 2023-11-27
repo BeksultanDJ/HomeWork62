991ce424716e18f8c079e9b8546aca1230aa4eea
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-27 16:57:24 +0600

    добавил контейнер с классом контейнер

--- src/componets/Tollbar/Toolbar.tsx
+++ src/componets/Tollbar/Toolbar.tsx
@@ -3,22 +3,27 @@ import {NavLink} from "react-router-dom";
 
 
 
+
 const Toolbar: React.FC = () => {
     return (
         <>
-            <nav>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/aboutUs">About Us</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/contacts">Contacts</NavLink>
-                    </li>
-                </ul>
-            </nav>
+            <header>
+                <div className="container">
+                    <nav>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="navLinks" to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="navLinks" to="/aboutUs">About Us</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="navLinks" to="/contacts">Contacts</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
         </>
     )
 }
